01e9529dbab6abb09f81ffefb34ef7e873512f79
Diego Sarmiento <60120140+diegosarmiento11@users.noreply.github.com>
2021-07-13 22:48:51 -0500

    EL, data, computed, methods and watchers were added to the project

---
+++ app.js
@@ -0,0 +1,47 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      name: 'Bitcoin',
+      symbol: 'BTC',
+      price: 8400,
+      color: 'f2f2f2',
+      value: 0,
+      image: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 10,
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+      ],
+      showPrices: false,
+    }
+  },
+  computed: {
+    title () {
+      return `${this.name} - ${this.symbol}`
+    },
+    convertedValue() {
+      if (!this.value) {
+        return 0
+      }
+
+      return this.value / this.price
+    }
+  },
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices
+      this.color = this.color.split('').reverse().join('')
+    }
+  }
+})
