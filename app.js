7c17bf9ce5b9f0501c011fd4dc379275ba071539
Diego Sarmiento <60120140+diegosarmiento11@users.noreply.github.com>
2021-07-15 00:59:53 -0500

    code refactoring and learning how to get data from parent components and modifying parent data from child using events

--- app.js
+++ app.js
@@ -1,47 +1,104 @@
-new Vue({
-  el: '#app',
-  data() {
+Vue.component('Coindetail', {
+  props: ['coin'],
+
+  data () {
     return {
-      name: 'Bitcoin',
-      symbol: 'BTC',
-      price: 8400,
-      color: 'f2f2f2',
-      value: 0,
-      image: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 10,
-      pricesWithDays: [
-        { day: 'Lunes', value: 8400 },
-        { day: 'Martes', value: 7900 },
-        { day: 'Miercoles', value: 8200 },
-        { day: 'Jueves', value: 9000 },
-        { day: 'Viernes', value: 9400 },
-        { day: 'Sabado', value: 10000 },
-        { day: 'Domingo', value: 10200 },
-      ],
       showPrices: false,
+      value: 0,
     }
   },
   computed: {
     title () {
-      return `${this.name} - ${this.symbol}`
+      return `${this.coin.name} - ${this.coin.symbol}`
     },
     convertedValue() {
       if (!this.value) {
         return 0
       }
 
-      return this.value / this.price
-    }
-  },
-  watch: {
-    showPrices(newVal, oldVal) {
-      console.log(newVal, oldVal)
+      return this.value / this.coin.price
     }
   },
   methods: {
     toggleShowPrices() {
       this.showPrices = !this.showPrices
+      this.$emit('change-color')
+    }
+  },
+  template: `
+    <div> 
+      <img 
+        v-bind:src='coin.image' 
+        v-bind:alt="coin.name">
+      <h1 
+        v-bind:class="coin.changePercent < 0 ?  'green' : 'red' ">  
+        {{ title }}
+        <span 
+          v-if='coin.changePercent < 0'>
+          👌🏻
+          </span>
+        <span 
+          v-else-if='coin.changePercent > 0'>
+          🥰 
+        </span>
+        <span 
+          v-else>
+          😭
+        </span>
+        <span 
+        v-on:click="toggleShowPrices"> 
+          {{ showPrices ? '🥰' : '🔑' }}
+        </span>
+      </h1>
+
+      <input 
+        type="number"
+        v-model="value">
+      <span>
+        {{ convertedValue }}
+      </span>
+      <ul 
+        v-show='showPrices'>
+        <li
+          class="uppercase" 
+          v-bind:class="{ orange: p.value === coin.price, red: p.value < coin.price, green: p.value > coin.price }"
+          v-for='(p,i) in coin.pricesWithDays' 
+          v-bind:key='p.day'>
+          {{ i }} - {{ p.value }} - {{ p.day }}
+        </li>
+      </ul>
+    </div>
+  `
+})
+ 
+new Vue({
+  el: '#app',
+  data() {
+    return {
+        btc: {
+          name: 'Bitcoin',
+          symbol: 'BTC',
+          price: 8400,
+          image: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+          changePercent: 10,
+          pricesWithDays: [
+          { day: 'Lunes', value: 8400 },
+          { day: 'Martes', value: 7900 },
+          { day: 'Miercoles', value: 8200 },
+          { day: 'Jueves', value: 9000 },
+          { day: 'Viernes', value: 9400 },
+          { day: 'Sabado', value: 10000 },
+          { day: 'Domingo', value: 10200 },
+        ]
+      },
+      color: 'f2f2f2',
+    }
+  },   
+  methods: {
+    updateColor() {
       this.color = this.color.split('').reverse().join('')
     }
   }
 })
+
+
